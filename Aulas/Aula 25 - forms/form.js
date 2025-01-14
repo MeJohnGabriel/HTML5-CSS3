8982b3841f21840a06c159e1896fa48da308d9bb
João Gabriel Reis Araújo <132390174+MeJohnGabriel@users.noreply.github.com>
2025-01-14 14:19:32 -0300

    finalizando cap forms
    finalizando cap forms e iniciando media queries

--- Aulas/Aula 25 - forms/form.js
+++ Aulas/Aula 25 - forms/form.js
@@ -1,1 +1,19 @@
-//DADOS ENVIADOS!
+"use strict";
+
+let number1 = document.getElementById("in1");
+let number2 = document.getElementById("in2");
+let result = document.getElementById("res");
+
+const sum = function () {
+  let n1 = Number(number1.value);
+  let n2 = Number(number2.value);
+  if (n1 == 0 && n2 == 0) {
+    window.alert("[ERROR] Fill the fields");
+  } else {
+    result.innerHTML = n1 + n2;
+  }
+};
+
+const test = function () {
+  result.innerHTML = 0;
+};
